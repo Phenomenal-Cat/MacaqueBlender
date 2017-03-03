18cf8a643412e9450adef9c81531129d50e18206
Aidan Murphy <murphyap@mail.nih.gov>
2017-03-03 18:28:42 -0500

    Automated target creation added

--- AddTargetObjects.py
+++ AddTargetObjects.py
@@ -1,19 +1,23 @@
 
-# AddTargetObjects.py
+#======= AddTargetObjects.py
 
 
 SphereDiam  = 0.04
+SphereLocs  = [(-0.2,0,0),(0,0.2,0),(0.2,0,0),(0,-0.2,0)];
+SphereNum   = [0,1,2,3]
 
-for sph in SphereLocs
+for sph in SphereNum:
+    print(SphereLoc[sph])
     bpy.ops.mesh.primitive_ico_sphere_add(
         subdivisions    = 100,
-        size            = SphereDiam
-        calc_uvs        = False
-        view_align      = False
-        enter_editmode  = False
-        location        = SphereLoc[sp]
+        size            = SphereDiam,
+        calc_uvs        = False,
+        view_align      = False,
+        enter_editmode  = False,
+        location        = SphereLoc[sph],
         rotation        = (0,0,0))
-    bpy.
+    #bpy.data.objects['Icosphere'].name = print("Target %d" % sph)    # Rename sphere as target ID
+        
     
     
     
