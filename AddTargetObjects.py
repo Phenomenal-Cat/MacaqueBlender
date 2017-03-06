16d98d6650cb0d7023a7a73f53fcd851e7bce602
Aidan <aidanmurphy1@hotmail.co.uk>
2017-03-06 08:56:45 -0500

    Updated add target

--- AddTargetObjects.py
+++ AddTargetObjects.py
@@ -1,23 +1,98 @@
 
-#======= AddTargetObjects.py
+#=================== EditTargetObjects.py =======================
+# These functions are used to add or remove target objects to or 
+# from the scene. 
+#
 
+import bpy
+import math
+import numpy as np
+import mathutils as mu
 
-SphereDiam  = 0.04
-SphereLocs  = [(-0.2,0,0),(0,0.2,0),(0.2,0,0),(0,-0.2,0)];
-SphereNum   = [0,1,2,3]
-
-for sph in SphereNum:
-    print(SphereLoc[sph])
-    bpy.ops.mesh.primitive_ico_sphere_add(
-        subdivisions    = 100,
-        size            = SphereDiam,
-        calc_uvs        = False,
-        view_align      = False,
-        enter_editmode  = False,
-        location        = SphereLoc[sph],
-        rotation        = (0,0,0))
-    #bpy.data.objects['Icosphere'].name = print("Target %d" % sph)    # Rename sphere as target ID
+def AddTargetObjects():
+    
+    #================= Set target appearance
+    SphereRad       = 0.01                                              # Set sphere radius (meters)
+    SphereDepth     = -0.2
+    SphereEcc       = 0.15
+    SphereNumber    = 8
+    SpherePolAng    = np.linspace(0,2*math.pi, SphereNumber+1)
+    SphereLocs      = np.zeros((SphereNumber,3))
+    for sph in range(0,SphereNumber):
+        SphereLocs[sph][0] = SphereEcc*math.sin(SpherePolAng[sph])
+        SphereLocs[sph][2] = SphereEcc*math.cos(SpherePolAng[sph])
+        SphereLocs[sph][1] = SphereDepth
         
+    SphereColor    = (0,0,1)                # Set target colors (RGB)
+
+    #================= Create material
+    AllMats = [mat for mat in bpy.data.materials if mat.name.startswith("Target")]
+    if all(AllMats):
+        mat                     = bpy.data.materials.new("TargetMat")       # Create new material
+        mat.diffuse_color       = SphereColor                               # Set material color
+        mat.specular_intensity  = 0                                         # Set intensity of specular reflection (0-1)
+    elif not(all(AllMats)):
+        mat = AllMats[0]
+
+    #================= Create targets
+    bpy.ops.group.create(name="Targets")
+    #bpy.ops.object.group_link(group="Targets")
+    for sph in range(0, len(SphereLocs)):
+        if sph == 0:
+            bpy.ops.mesh.primitive_ico_sphere_add(
+                subdivisions    = 100,
+                size            = SphereRad,
+                calc_uvs        = False,
+                view_align      = False,
+                enter_editmode  = False,
+                location        = SphereLocs[sph],
+                rotation        = (0,0,0))
+            bpy.data.objects['Icosphere'].active_material   = mat                           # Set target color
+            bpy.data.objects['Icosphere'].name              = "Target %d" % sph             # Rename object as target ID
+        elif sph  > 0:
+            d = bpy.data.objects['Target 0'].copy()
+            bpy.context.scene.objects.link(d)
+            d.location      = SphereLocs[sph]
+            d.name          = "Target %d" % sph  
+            
+            
+    bpy.context.scene.objects["Target %d"].select = True
+    bpy.ops.object.group_link(group="Targets")    
+    bpy.context.scene.objects["Target %d"].select = False     
+    #bpy.context.scene.objects.active = d
+    #bpy.ops.group.objects_add_active(group = "Targets")
+    return SphereLocs
+        
+        
+def RemoveTargetObjects():
+    scene        = bpy.context.scene
+    AllTargets   = [obj for obj in scene.objects if obj.name.startswith("Target")]  # Find all objects in scene with 'Target' in name
+    for t in range(0,len(AllTargets)):
+        AllTargets[t].select = True         # Select next target object
+        bpy.ops.object.delete()             # Delete target object
+        
+
+def EditTargetObjects(Add):
+    
+    if Add == 1:
+        Locs = AddTargetObjects()
+    elif Add == 0:
+        RemoveTargetObjects()
+        
+def MonkeyLookAt(HeadLoc, GazeLoc):
+    bpy.data.objects["HeaDRig"].pose.bones['EyesTracker'].location = mu.Vector((GazeLoc)) - bpy.data.objects["HeaDRig"].location
+    bpy.data.objects["HeaDRig"].pose.bones['HeadTracker'].location = mu.Vector((HeadLoc)) - bpy.data.objects["HeaDRig"].location
+        
+        
+        
+#RemoveTargetObjects()
+#Locs = AddTargetObjects()
+
+Loc = [0.15,0.2,0]
+MonkeyLookAt(Loc, Loc)
+    
+
+
     
     
     
